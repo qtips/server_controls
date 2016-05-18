a57d8a7c436fe6866d8545acb215bf5d39c76021
qtips <ahm.qadeer@gmail.com>
2016-05-18 23:26:15 +0200

    started embedded jetty server using annotated servlet - had to point the webinf directory to class current dir in order to scan annotated classes

--- src/main/java/KodiControlPanelServer.java
+++ src/main/java/KodiControlPanelServer.java
@@ -1,11 +1,44 @@
-import java.io.IOException;
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.*;
+
+import java.net.URL;
+import java.util.Arrays;
 
 public class KodiControlPanelServer {
 
 
-    public static void main(String[] args) throws IOException {
-//    Server server = new Server(8080);
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
 
-        new Rebooter().reboot();
+//        new Rebooter().reboot();
+
+        URL classes = KodiControlPanelServer.class
+                .getProtectionDomain()
+                .getCodeSource()
+                .getLocation();
+
+
+        WebAppContext context = new WebAppContext();
+        context.setResourceBase("/");
+        context.getMetaData()
+                .setWebInfClassesDirs(
+                        Arrays.asList(Resource.newResource(classes)));
+//        ServletContextHandler handler = new ServletContextHandler();
+//        handler.addServlet(new ServletHolder(new ControlPanelServlet()));
+
+        context.setConfigurations(new Configuration[]{
+                new AnnotationConfiguration(), new WebXmlConfiguration(),
+                new WebInfConfiguration(),
+                new PlusConfiguration(), new MetaInfConfiguration(),
+                new FragmentConfiguration(), new EnvConfiguration()});
+
+
+        server.setHandler(context);
+        server.start();
+        server.join();
     }
 }
